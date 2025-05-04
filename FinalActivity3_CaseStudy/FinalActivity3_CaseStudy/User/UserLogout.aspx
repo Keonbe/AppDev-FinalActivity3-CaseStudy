048b0e7481509d15a808515aa48d5ba09884e80d
Keonbe <Keonbe@users.noreply.github.com>
2025-05-04 12:55:21 +0800

    Add Logout page; Working. Read Comment on this for "cd' navigation

---
+++ FinalActivity3_CaseStudy/FinalActivity3_CaseStudy/User/UserLogout.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User/MainMasterpage.Master" AutoEventWireup="true" CodeBehind="UserLogout.aspx.cs" Inherits="FinalActivity3_CaseStudy.User.UserLogout" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        Logout</p>
+    <table class="auto-style1">
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>Confirm Logout?</td>
+            <td>
+                <asp:Button ID="btnLogout" runat="server" Text="Logout" OnClick="btnLogout_Click" />
+            </td>
+        </tr>
+    </table>
+    <p>
+        <asp:Label ID="lblMessage" runat="server"></asp:Label>
+    </p>
+    <p>
+        &nbsp;</p>
+</asp:Content>
---
+++ FinalActivity3_CaseStudy/FinalActivity3_CaseStudy/User/UserLogout.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalActivity3_CaseStudy.User
+{
+    public partial class UserLogout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Check if the user is already logged in
+                if (Session["EmailAddress"] == null)
+                {
+                    // If not logged in, Redirect and Display message
+                    lblMessage.Text = "⚠️ Not logged in - Redirecting to Login Page";
+                    btnLogout.Visible = false; // If not login, Hide the logout button
+                    Response.AppendHeader("Refresh", "3;url=Login.aspx");
+                }
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Clear();        // Clear session data
+            Session.Abandon();      // Ends session
+            lblMessage.Text = "Account Successfully Logged Out. Redirecting to Home Page...";
+            Response.AppendHeader("Refresh", "2;url=/Homepage.aspx"); //Redirect to homepage.aspx
+            // Use "/webform.aspx" or "~/folder/webform.aspx" for specific webform @ admin or user
+        }
+    }
+}
